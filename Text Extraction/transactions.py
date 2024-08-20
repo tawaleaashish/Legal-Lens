a98a20666fe53053819a0832768bfaea841db63a
shrikhandeatharva <atharvashrikhande777@gmail.com>
2024-08-20 15:07:48 +0530

    text extraction

---
+++ Text Extraction/transactions.py
@@ -0,0 +1,7 @@
+import pymupdf
+import pymupdf4llm
+
+doc=pymupdf.open("E:\Mega Project\constitution of india 3.pdf")
+md=pymupdf4llm.to_markdown(doc)
+with open("temp.md", "w", encoding="utf-8") as f:
+    f.write(md)
