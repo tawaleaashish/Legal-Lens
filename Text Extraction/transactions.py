9766765ab6cb80cd558c70473db4395850750071
shrikhandeatharva <atharvashrikhande777@gmail.com>
2024-08-20 19:52:43 +0530

    added a comment

--- Text Extraction/transactions.py
+++ Text Extraction/transactions.py
@@ -5,3 +5,4 @@ doc=pymupdf.open("E:\Mega Project\constitution of india 3.pdf")
 md=pymupdf4llm.to_markdown(doc)
 with open("temp.md", "w", encoding="utf-8") as f:
     f.write(md)
+    # asdadewfcfvdsfc
